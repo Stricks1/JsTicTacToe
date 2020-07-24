ba585641346d8461301fff83c79fa0de4e9c6ad6
Stricks1 <gmalheiross@gmail.com>
2020-07-24 19:17:20 -0300

    styling gameboard to display on screen

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const gameBoard = (() => {
-  const board = ['','X','','X','O','O','O','X','']
+  const board = ['','','','','','','','','']
   const render = () => {
     let cells = document.getElementsByClassName('cell');
     console.log(cells);
     for (let i = 0; i < cells.length; i++) {
       cells[i].innerHTML = board[i];
-      console.log(cells[i]); //second console output
     }
     
   }
